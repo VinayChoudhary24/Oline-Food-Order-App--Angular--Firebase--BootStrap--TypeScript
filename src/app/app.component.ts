5385c1f19112da830914b6f7faa771fe7e3505d4
VinayPsych <vinaychoudhary994@gmail.com>
2022-10-07 14:41:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+//## To Use BootStrap we need to Inform Angular in ANGULAR.JSON File in STYLES[]
+   //-- "node_modules/bootstrap/dist/css/bootstrap.min.css"
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'online-food-order-app';
 }
