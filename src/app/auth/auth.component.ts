ed92f383e57fdd19503b961561e5395aacff4a72
VinayPsych <vinaychoudhary994@gmail.com>
2022-11-06 21:16:01 +0530

    Login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
@@ -10,6 +11,14 @@ export class AuthComponent {
     // To switch between the User
     isLoginMode = true;
 
+    // This is for the Loading-Spinner
+    isLoading = false;
+
+    // This is for the ERROR
+    error: string = null;
+
+    constructor( private authService: AuthService ) {}
+
     // Method to swith Modes
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -17,7 +26,30 @@ export class AuthComponent {
 
     // This will Submit the Form
     onSubmit(form: NgForm) {
-        console.log(form.value);
+        // console.log(form.value);
+        if(!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+
+        // to show the Loading-Spinner Here
+        this.isLoading = true;
+        // 
+        if(this.isLoginMode) {
+
+        } else {
+        // Subscribe the SignUp POST Request Here
+        this.authService.signUp(email, password).subscribe( (resData) => {
+            console.log(resData);
+            this.isLoading = false;
+        }, (errorMessage) => {
+            console.log(errorMessage);
+            this.error = errorMessage;
+            this.isLoading = false;
+        });
+        }
+
         form.reset();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, throwError } from "rxjs";
 
 // This is the Response that we will get from FIREBASE after the SignUp/Login Request
+// The Response Data for SignUp and Login is almost same, with the ONLY difference is the REGISTERED key which is the response for the Login
 interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    // Add a ? with this key i.e making it Optional because it's present in Login and Not in SignUp ResponseData
+    registered?: boolean;
 }
 
 @Injectable({
@@ -20,6 +24,29 @@ export class AuthService {
 
     signUp(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDmmW0q9rEh-BDoOXe80JUkqKZQ5KTxAgo', {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        })
+        // Errors are Handles Here
+        .pipe(catchError( (errorRes) => {
+            let errorMessage = 'An Unknown error occured!';
+            if(!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage);
+            }
+             // For Different Types of errors we Use Switch Case
+             switch(errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                    errorMessage = 'This email exists already.'
+                // case 'OPERATION_NOT_FOUND':
+                //     errorMessage = 'This is Invalid.'
+            }
+            return throwError(errorMessage);
+        }))
+    }
+    // Login Process
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDmmW0q9rEh-BDoOXe80JUkqKZQ5KTxAgo', {
             email: email,
             password: password,
             returnSecureToken: true
